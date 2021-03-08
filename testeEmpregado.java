ad9518825c78aee85125354a7631b6f9c34347a0
vinivizzapc <vini_vizza@hotmail.com>
2021-03-08 20:32:49 -0300

    Criação da classe

---
+++ testeEmpregado.java
@@ -0,0 +1,7 @@
+public class TesteEmpregado{
+     public static void main(String[] args){
+         Empregado e1 = new empregado();
+         Empregado e2 = new empregado();    
+         Empregado e3 = new empregado();
+    }
+}
