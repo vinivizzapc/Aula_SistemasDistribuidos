6269f5de396ab70157abb2f7532cdb07a9db150e
vinivizzapc <vini_vizza@hotmail.com>
2021-03-08 19:55:52 -0300

    Commit inicial. Classe Empregado criada

---
+++ empregado.java
@@ -0,0 +1,20 @@
+public class empregado {
+    private String nome;
+    private int idade;
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
